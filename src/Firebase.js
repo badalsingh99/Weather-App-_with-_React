0539bd4772414e5b6947ac542fe8ce18af772a55
Badal Singh <93906741+badalsingh99@users.noreply.github.com>
2024-01-22 18:57:44 +0530

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyB50AgrIRO5zQFWImOgAO2kDFAGaAzA1ok",
   authDomain: "weather-ae5dc.firebaseapp.com",
@@ -17,7 +13,7 @@ const firebaseConfig = {
   measurementId: "G-CYLRBW8B67"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export default app;
